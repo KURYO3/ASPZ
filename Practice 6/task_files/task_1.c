17ffee777c18ce4d746978204e2106c1d37c3c92
Oleksandr Kozachenko <kozachenko.oleksandr@lll.kpi.ua>
2025-05-17 18:11:31 +0300

    Add files via upload

---
+++ Practice 6/task_files/task_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int *arr = malloc(5 * sizeof(int));
+
+	for (int i = 0; i < 5; ++i) {
+		arr[i] = i * 10;
+	}
+
+	int val = arr[7];
+
+	printf("Read value: %d\n", val);
+
+	free(arr);
+	return 0;
+}
